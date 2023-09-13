d337a0edc40e3412d39f43c0b4629e89c961c05f
Divyam <dk0864@srmist.edu.in>
2023-09-13 14:47:15 +0530

    solution

---
+++ Substraction.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+class Substraction
+{
+   
+    public static void main (String args[]){
+         double num1,num2;double sub;
+        Scanner br = new Scanner(System.in);
+        System.out.println("enter 1st number");
+        num1=br.nextDouble();
+        System.out.println("enter 2nd number");
+        num2=br.nextDouble();
+        sub=num1-num2;
+        System.out.println("The difference is :"+sub);
+
+
+    }
+
+}
+
