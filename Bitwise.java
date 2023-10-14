42f28a2b3b1fe8226e6ba146a0057b5fb641418c
Divyam <dk0864@srmist.edu.in>
2023-10-14 20:31:36 +0530

    Create Bitwise.java

---
+++ Bitwise.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+class Bitwise
+{
+public static void main(String args[])
+{
+    int x=0b1010;
+    int y=0b0110;
+    int z;
+    int a;
+
+    z = x&y;
+    a = x|y;
+    System.out.println(a);
+    System.out.println(z);
+}
+}
+
