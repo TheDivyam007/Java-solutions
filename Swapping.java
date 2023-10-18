43e9b8e74b71f06103b82a20a940b353957621e4
Divyam <dk0864@srmist.edu.in>
2023-10-18 21:32:58 +0530

    Swap without third var

---
+++ Swapping.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+
+public class Swapping {
+    public static void main(String args[]){
+        int a = 10, b = 15;
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println(a+" "+b);
+    }                                                                                     
+    
+}
